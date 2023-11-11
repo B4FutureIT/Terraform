2999a05276e746317edb1b4c39a7c9bbad0c1452
Lukasz Piskorz <b4futureai@gmail.com>
2023-11-11 15:12:40 +0100

    Title: Add HTTP Server on Port 8080 and Open Ports for HTTP and SSH
    Description:
    - Implemented a simple HTTP server that serves content on port 8080.
    - Updated the firewall settings to allow inbound traffic on port 8080 and 22.

--- MyEnv1/main.tf
+++ MyEnv1/main.tf
@@ -3,10 +3,35 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-06d1e80aa22f0ebac"
-  instance_type = "t2.micro"
+  #  ami                    = "ami-06d1e80aa22f0ebac"
+  ami                    = "ami-06d1e80aa22f0ebac"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Welcome in my world ;)" > index.html
+              nohup python3 -m http.server 8080 &
+              EOF
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-example-new"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-examaple-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
