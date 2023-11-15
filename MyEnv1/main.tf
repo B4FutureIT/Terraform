3f904e02c0a1db8282180f86e4a53ebd810665a2
Lukasz Piskorz <b4futureai@gmail.com>
2023-11-15 17:10:28 +0100

    Adding scalling resources by index and index.count variable

--- MyEnv1/main.tf
+++ MyEnv1/main.tf
@@ -7,14 +7,16 @@ resource "aws_instance" "example" {
   ami                    = "ami-06d1e80aa22f0ebac"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+  count                  = 2 # Testing for maximum 20 instances
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Welcome in my world ;)" > index.html
               nohup python3 -m http.server 8080 &
               EOF
+
   tags = {
-    Name = "terraform-example-new"
+    Name = "linux${count.index}"
   }
 }
 
